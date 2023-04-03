bef1d60ac6038a9f99a2b20c5f2a733251683138
Ursulina Kölbener <ursulina@koelbener.ch>
2023-04-03 13:03:29 +0200

    adds script, asks fo API key

---
+++ scripts/01_corpus-of-articles.R
@@ -0,0 +1,16 @@
+###################################################################
+# Class Exercise 2
+# Sascha Eng & Ursulina Kölbener
+###################################################################
+
+# 1. install and load packages ----
+# install.packages("httr")
+# install.packages("jsonlite")
+
+library(httr)
+library(jsonlite)
+library(tidyverse)
+
+# 2. save personal API key ----
+api_key <- rstudioapi::askForPassword()
+# make sure the key is not pushed to github
