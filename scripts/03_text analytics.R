e4aeb1ce9ab80d2726fe76a0e8403c3ad946eb8d
Ursulina Kölbener <ursulina@koelbener.ch>
2023-04-21 09:32:48 +0200

    create corpus

--- scripts/03_text analytics.R
+++ scripts/03_text analytics.R
@@ -6,8 +6,16 @@
 ## TEXT ANALYTICS
 
 # 1. preparations ----
-# install.packages("quanteda")
+install.packages("quanteda")
+
 library(quanteda)
 
 # load dataframe
-data.table::fread(df_articles, here::here("data", "df_articles.csv"))
+df_articles <- data.table::fread(here::here("data", "df_articles.csv"))
+
+# create unique document names
+docnames <- paste(df_articles$id, seq_along(df_articles$id), sep = "_")
+
+# 2. create a corpus wit the main texts ----
+corp_main <- corpus(df_articles$body_text, docnames = docnames)
+
