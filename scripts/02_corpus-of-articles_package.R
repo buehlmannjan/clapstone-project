546e01fc8da044ad6625aa80e725e13b9bfc851b
buehlmannjan <hello@janbuehlmann.com>
2023-04-21 09:30:29 +0200

    Sentiment analysis

--- scripts/02_corpus-of-articles_package.R
+++ scripts/02_corpus-of-articles_package.R
@@ -9,8 +9,10 @@
 # install.packages("devtools") 
 # install.packages("tm")
 # yeyinstall.packages("stringr")
+install.packages("ggplot2")
 
-library(httr)
+
+#library(httr)
 library(jsonlite)
 library(tidyverse)
 library(devtools)
@@ -35,21 +37,21 @@ print(chatgpt)
 head(chatgpt)
 colnames(chatgpt)
 
-# Display the structure of the dataset
+# 4. Display the structure of the dataset
 str(chatgpt)
 
-# Generate summary statistics for numeric columns
+# a) Generate summary statistics for numeric columns
 summary(chatgpt)
 
-# Load necessary libraries
+# b) Load necessary libraries
 library(ggplot2)
 library(lubridate)
 
-# Calculate the frequency of articles by month
+# c) Calculate the frequency of articles by month
 chatgpt$month <- floor_date(chatgpt$web_publication_date, "month")
 monthly_counts <- as.data.frame(table(chatgpt$month))
 
-# Plot the distribution of publication dates
+# d) Plot the distribution of publication dates
 ggplot(monthly_counts, aes(x = Var1, y = Freq)) +
   geom_col() +
   labs(x = "Publication Month", y = "Frequency", title = "Distribution of Article Publication Dates") +
@@ -59,5 +61,81 @@ ggplot(monthly_counts, aes(x = Var1, y = Freq)) +
 
 print(monthly_counts)
 
-# 3. Preprocessing
+# 5. Preprocessing
 ## a. Clean the text data.
+
+# Define the cleaning function
+clean_text <- function(text) {
+  library(stringr)
+  library(tm)
+  
+  cleaned_text <- str_replace_all(text, "<[^>]*>", "") # Remove HTML tags
+  cleaned_text <- gsub("[^[:alnum:]///' ]", "", cleaned_text) # Remove special characters
+  return(cleaned_text)
+}
+
+# Print the column names of the dataset
+colnames(chatgpt)
+
+
+# Apply the cleaning function to the bodytext column
+chatgpt$body_text_cleaned <- clean_text(chatgpt$body_text)
+
+## b. Tokenize the words.
+
+# Load necessary libraries
+library(tidytext)
+library(dplyr)
+
+# Tokenize the words
+chatgpt_tidy <- chatgpt %>%
+  unnest_tokens(word, bodytext_cleaned)
+
+## c. Remove stopwords.
+
+# Load the stopwords
+data("stop_words")
+
+# Remove stopwords
+chatgpt_tidy <- chatgpt_tidy %>%
+  anti_join(stop_words)
+
+## d. Count the frequency of words.
+
+word_counts <- chatgpt_tidy %>%
+  count(word, sort = TRUE)
+
+## e. Visualize the most frequent words.
+
+# Plot the top 10 most frequent words
+word_counts %>%
+  top_n(10) %>%
+  ggplot(aes(x = reorder(word, n), y = n)) +
+  geom_col() +
+  coord_flip() +
+  labs(x = "Words", y = "Frequency", title = "Top 10 Most Frequent Words") +
+  theme_minimal()
+
+
+
+# 6. Assuming chatgpt_sentiment has a date column named 'date'
+library(lubridate)
+
+# Calculate daily average sentiment
+
+
+# install.packages("syuzhet")
+library(syuzhet)
+
+# Calculate sentiment scores
+chatgpt_sentiment <- chatgpt %>%
+  select(web_publication_date, body_text_cleaned) %>%
+  mutate(sentiment = get_sentiment(body_text_cleaned, method = "nrc")) %>%
+  mutate(date = floor_date(web_publication_date, "day")) %>%
+  group_by(date) %>%
+  summarize(avg_sentiment = mean(sentiment))
+
+ggplot(chatgpt_sentiment, aes(x = date, y = avg_sentiment)) +
+  geom_line() +
+  labs(x = "Date", y = "Average Sentiment", title = "Daily Average Sentiment of Articles") +
+  theme_minimal()
